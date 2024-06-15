15d894ce0c8708d874570f27900b16144f173c83
Omnikeck <dustinohlemann@gmail.com>
2024-06-15 22:44:57 +0200

    eeee

--- src/main/java/dev/aithon/config/ConfigManager.java
+++ src/main/java/dev/aithon/config/ConfigManager.java
@@ -6,20 +6,25 @@ import org.bukkit.plugin.Plugin;
 import java.io.File;
 
 public class ConfigManager {
-
-    private YamlConfiguration yamlConfiguration;
-    private File file;
-
+    private final File file;
+    private final YamlConfiguration yamlConfiguration;
 
     public ConfigManager(String fileName, Plugin plugin){
         this.file = new File(fileName);
-        if(!file.exists()){
+        this.yamlConfiguration = YamlConfiguration.loadConfiguration(file);
+        if(!this.file.exists()){
             plugin.saveResource(fileName, false);
         }
-        this.yamlConfiguration = YamlConfiguration.loadConfiguration(file);
     }
 
     public <T> T parse(Class <T> o, String path) {
         return o.cast(yamlConfiguration.get(path));
+    }
+
+    public static void createFile(String fileName, Plugin plugin) {
+        File file = new File(fileName);
+        if (!file.exists()) {
+            plugin.saveResource(fileName, false);
+        }
     }
 }
